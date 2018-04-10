05dc10898f64260b7ce7506a8dbf630d7e853c1b
Brandon Meeks <brandon@desine.co>
2018-04-10 17:51:15 -0400

    Updated to rails 5.1.6

--- db/migrate/20151024151120_add_username_to_users.rb
+++ db/migrate/20151024151120_add_username_to_users.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUsers < ActiveRecord::Migration
+class AddUsernameToUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :people, :username, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,18 +13,18 @@
 ActiveRecord::Schema.define(version: 20151024151120) do
 
   create_table "people", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "role"
-    t.string   "username"
+    t.string "email"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role"
+    t.string "username"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
